ffe3e3a39ba2b11574b292af3eaa0a9e65189502
Daniel Filho <drcfilho@hotmail.com>
2026-09-04 10:23:49 -0300

    fix(service): adiciona import faltante de CouponRepository em OrderService

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Helpers\TenantResolver;
+use App\Repositories\CouponRepository;
 use App\Repositories\NeighborhoodRepository;
 use App\Repositories\PaymentMethodRepository;
 use App\Repositories\ProductRepository;
